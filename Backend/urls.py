1525d93d45295013f38f02352e43d08628596975
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-12-31 22:49:46 +0530

    adding documentation

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path, include
 from .views import (
     Clubs,
@@ -13,7 +14,27 @@ from .views import (
     Posts
 )
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Gaze NITH Api",
+        default_version='v1',
+        description="Api To get details for NITH And various social activities",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mrigank.anand52@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('clubs/', Clubs.as_view()),
     path('sports/', Sports.as_view()),
     path('shops/', Shops.as_view()),
--- Orientation_app_backend/settings.py
+++ Orientation_app_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'Backend',
+    'drf_yasg',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
